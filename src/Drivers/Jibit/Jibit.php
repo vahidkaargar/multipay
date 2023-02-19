37103a14dd67b78248c4c4f2d2181f58d586b1b7
Vahid Kaargar <vahidkaargar@gmail.com>
2023-02-20 01:36:27 +0330

    Create Jibit driver
    jibit.ir

---
+++ src/Drivers/Jibit/Jibit.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Shetabit\Multipay\Drivers\Jibit;
+
+use GuzzleHttp\Client;
+use Shetabit\Multipay\Abstracts\Driver;
+use Shetabit\Multipay\Exceptions\InvalidPaymentException;
+use Shetabit\Multipay\Exceptions\PurchaseFailedException;
+use Shetabit\Multipay\Contracts\ReceiptInterface;
+use Shetabit\Multipay\Invoice;
+use Shetabit\Multipay\Receipt;
+use Shetabit\Multipay\RedirectionForm;
+use Shetabit\Multipay\Request;
+
+class Jibit extends Driver
+{
+    /**
+     * Jibit Client.
+     *
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * Invoice
+     *
+     * @var Invoice
+     */
+    protected $invoice;
+
+    /**
+     * Driver settings
+     *
+     * @var object
+     */
+    protected $settings;
+
+    /**
+     * Jibit constructor.
+     * Construct the class with the relevant settings.
+     *
+     * @param Invoice $invoice
+     * @param $settings
+     */
+    public function __construct(Invoice $invoice, $settings)
+    {
+        $this->invoice($invoice);
+        $this->settings = (object)$settings;
+        $this->client = new Client();
+    }
+
+    public function purchase()
+    {
+        // TODO: Implement purchase() method.
+    }
+
+    public function pay(): RedirectionForm
+    {
+        // TODO: Implement pay() method.
+    }
+
+    public function verify(): ReceiptInterface
+    {
+        // TODO: Implement verify() method.
+    }
+}
